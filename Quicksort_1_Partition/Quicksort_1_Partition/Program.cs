b04493e6ac36e5541e9e5f3cff42c584710f88a8
Felippe Borello <fborello@brasilplural.com>
2017-08-09 15:40:00 -0300

    no message

---
+++ Quicksort_1_Partition/Quicksort_1_Partition/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quicksort_1_Partition
+{
+    class Program
+    {
+        static void partition(int[] ar)
+        {
+            int p = ar[0];
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            List<int> equal = new List<int>();
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] == p)
+                    equal.Add(ar[i]);
+                else if (ar[i] > p)
+                    right.Add(ar[i]);
+                else if (ar[i] < p)
+                    left.Add(ar[i]);
+            }
+
+            foreach (int item in left)
+                Console.Write(item.ToString() + " ");
+            foreach (int item in equal)
+                Console.Write(item.ToString() + " ");
+            foreach (int item in right)
+                Console.Write(item.ToString() + " ");
+
+        }
+        /* Tail starts here */
+        static void Main(String[] args)
+        {
+
+            int _ar_size;
+            _ar_size = Convert.ToInt32(Console.ReadLine());
+            int[] _ar = new int[_ar_size];
+            String elements = Console.ReadLine();
+            String[] split_elements = elements.Split(' ');
+            for (int _ar_i = 0; _ar_i < _ar_size; _ar_i++)
+            {
+                _ar[_ar_i] = Convert.ToInt32(split_elements[_ar_i]);
+            }
+
+            partition(_ar);
+        }
+    }
+
+}
+
---
+++ Two_Characters/Two_Characters/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Characters
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int len = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            List<char> listChar = new List<char>();
+
+
+            int max = 0;
+            string newSTR = RemoveChar(s);
+
+            foreach (char c in newSTR)
+            {
+                if (!listChar.Contains(c))
+                    listChar.Add(c);
+            }
+            for (int i = 0; i < listChar.Count; i++)
+            {
+                for (int j = 1; j < listChar.Count; j++)
+                {
+                    string a = RemoveChars(newSTR, listChar[i], listChar[j]);
+                    bool validated = Validate(a);
+                    if (validated && a.Length > max)
+                    {
+                        max = a.Length;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(max.ToString());
+        }
+
+        private static string RemoveChars(string s, char v1, char v2)
+        {
+            string aaa = s;
+
+            foreach (char c in s)
+            {
+                if (c != v1 && c != v2)
+                    aaa = aaa.Replace(c.ToString(), "");
+            }
+            return aaa;
+        }
+
+        static bool Validate(string str)
+        {
+            bool find = false;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == str[i - 1])
+                {
+                    find = true;
+                    str = str.Replace(str[i].ToString(), "");
+                    break;
+                }
+            }
+
+            return !find;
+        }
+
+        static string RemoveChar(string str)
+        {
+            bool find = false;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == str[i - 1])
+                {
+                    find = true;
+                    str = str.Replace(str[i].ToString(), "");
+                    break;
+                }
+            }
+            if (find)
+                return RemoveChar(str);
+
+            return str;
+        }
+    }
+}
