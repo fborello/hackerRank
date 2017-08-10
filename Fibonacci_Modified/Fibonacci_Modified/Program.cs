8694cc1b913509874d48969de3327e5dac097139
fborello-adm <fborello-adm@LRIO0210100>
2017-08-10 15:05:06 -0300

    no message

---
+++ Fibonacci_Modified/Fibonacci_Modified/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Modified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+            string[] str = Console.ReadLine().Split(' ');
+
+            List<decimal> list = new List<decimal>();
+            list.Add(decimal.Parse(str[0]));
+            list.Add(decimal.Parse(str[1]));
+            decimal m = decimal.Parse(str[2]);
+
+            for (int i = 2; i < m; i++)
+            {
+                decimal next = list[i - 2] + (list[i - 1] * list[i - 1]);
+                list.Add(next);
+            }
+
+            Console.WriteLine(list[list.Count - 1].ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mark_and_Toys/Mark_and_Toys/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mark_and_Toys
+{
+    class Program
+    {
+        static int maximumToys(int[] prices, int k)
+        {
+            // Complete this function
+
+            List<int> list = prices.ToList();
+            list.Sort();
+
+            int count = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] <= k)
+                {
+                    k -= list[i];
+                    count++;
+                }
+                else
+                    break;
+            }
+            return count;
+        }
+
+        static void Main(String[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] prices_temp = Console.ReadLine().Split(' ');
+            int[] prices = Array.ConvertAll(prices_temp, Int32.Parse);
+            int result = maximumToys(prices, k);
+            Console.WriteLine(result);
+
+        }
+    }
+}
