60d9efa6e69dfd46f69a30d02033a8cb2b35113f
fborello-adm <fborello-adm@LRIO0210100>
2017-08-15 17:52:55 -0300

    no message

---
+++ Stock_Maximize/Stock_Maximize/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Maximize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+            int T = int.Parse(Console.ReadLine());
+            List<long> returnList = new List<long>();
+            for (int i = 0; i < T; i++)
+            {
+                int N = int.Parse(Console.ReadLine());
+                long[] array = new long[N];
+                string[] item = Console.ReadLine().ToString().Split(' ').ToArray();
+                for (int j=0;j<N;j++)
+                {
+                    long l = long.Parse(item[j]);
+                    array[j] = l;
+                }
+                Console.WriteLine(gain(array));
+                
+            }
+
+            Console.ReadLine();
+        }
+
+        public static long gain(long[] li)
+        {
+            long gain = 0;
+            long max = 0;
+            for(int i = li.Length-1;i>=0;i--)
+            {
+                if (li[i] > max)
+                    max = li[i];
+                gain += max - li[i];
+            }
+
+            return gain;
+        }
+        
+    }
+
+}
---
+++ The_Power_Sum/The_Power_Sum/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Power_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+
+            decimal first = 1;
+            decimal v = first / N;
+
+            double maxNum = Math.Pow(X, (double)v);
+
+            double maxNum2 = Math.Truncate(maxNum);
+
+            decimal count = 0;
+
+            count += SumOrNot(maxNum2, X, N, v);
+
+            Console.WriteLine(count.ToString());
+            Console.ReadLine();
+        }
+
+        private static int SumOrNot(double maxNum2, int X, int N, decimal v)
+        {
+            int count = 0;
+            double sum = 0;
+            for (int i = (int)maxNum2; i > 0; i--)
+            {
+                sum += Math.Pow(i, N);
+                if (sum == X)
+                    count += 1 + SumOrNot(maxNum2 - 1, X, N, v);
+                else if (sum > X)
+                    count += SumOrNot(maxNum2 - 1, X, N, v);
+                else
+                {
+                    double rest = X - sum;
+
+                    double restNum = Math.Pow(rest, (double)v);
+
+                    double restNum2 = Math.Truncate(restNum);
+                    if (i != 1)
+                        i = (int)restNum2 + 1;
+                }
+
+            }
+
+            if(maxNum2 != 1)
+                count += SumOrNot(maxNum2 - 1, X, N, v);
+
+            return count;
+        }
+    }
+}
+
